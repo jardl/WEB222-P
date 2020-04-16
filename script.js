175566dd8966a3179dbd1c82dee2b82e11ffcdd2
Spectrex551 <jared.lecesne@gmail.com>
2020-04-16 14:16:35 -0500

    hi

--- script.js
+++ script.js
@@ -1,55 +1,127 @@
-/** Add any JavaScript you need to this file. */
+//Test
 
 (function() {
-  let loadItems = function(item, prices, names, desc, qty) {
-    let products = document.querySelector('.productsContent');
-    let source = 'images/';
+    let loadItems = function(item, prices, names, desc, qty) {
+        let products = document.querySelector('#products');
+        let source = 'images/';
+    
+        for (let x = 0; x < qty; x++) {
+            let cardBox = document.createElement('div');
+            cardBox.setAttribute('class', 'col-lg-4 col-md-6 mb-4');
+        
+            let card = document.createElement('div');
+            card.setAttribute('class', 'card h-100');
 
-    for (let x = 0; x < qty; x++) {
-      let cardBox = document.createElement('div');
-      cardBox.setAttribute('class', 'cardBox');
+            let link=document.createElement('a');
+            link.setAttribute('href', '#');
+        
+            let img = document.createElement('img');
+            let src = source + item + x + '.jpeg';
+            img.setAttribute('src', src);
+            img.setAttribute('class', 'card-img-top');
+            src = item + x;
+            img.setAttribute('alt', src);
+            link.appendChild(img);
 
-      let card = document.createElement('div');
-      card.setAttribute('class', 'card');
+            let cardBody = document.createElement('div');
+            cardBody.setAttribute('class', 'card-body');
 
-      let img = document.createElement('img');
-      let src = source + item + x + '.jpeg';
-      img.setAttribute('src', src);
-      src = item + x;
-      img.setAttribute('alt', src);
-      img.setAttribute('style', 'width: 100%');
+            let namebox = document.createElement('h4');
+            namebox.setAttribute('class', 'card-title');
 
-      let bottle1 = document.createTextNode(names[x]);
-      let name = document.createElement('h2');
-      name.appendChild(bottle1);
+            let itemname = document.createElement('a');
+            itemname.setAttribute('href', '#');
+            let text = document.createTextNode(names[x]);
+            itemname.appendChild(text)
 
-      let text = document.createElement('p');
-      text.setAttribute('class', 'price');
-      let temp1 = document.createTextNode(prices[x]);
-      text.appendChild(temp1);
+            namebox.appendChild(itemname);
 
-      let text2 = document.createElement('p');
-      text2.setAttribute('class', 'info');
-      let temp3 = document.createTextNode(desc[x]);
-      text2.appendChild(temp3);
+            let pricetag=document.createElement('h5');
+            let pricevalue=document.createTextNode(prices[x]);
+            pricetag.appendChild(pricevalue);
 
-      let press = document.createElement('p');
-      let button = document.createElement('button');
-      let temp2 = document.createTextNode('Add to Cart');
-      button.appendChild(temp2);
-      press.appendChild(button);
+            let descBox = document.createElement('p');
+            descBox.setAttribute('class', 'card-text')
+            let description = document.createTextNode(desc[x]);
+            descBox.appendChild(description);
 
-      card.appendChild(img);
-      card.appendChild(name);
-      card.appendChild(text);
-      card.appendChild(text2);
-      card.appendChild(press);
-      cardBox.appendChild(card);
-      products.appendChild(cardBox);
+
+
+        
+            let cardFooter = document.createElement('div');
+            cardFooter.setAttribute('class', 'card-footer');
+
+            let small = document.createElement('small');
+            small.setAttribute('class', 'text-muted');
+            small.innerHTML ="&#9733; &#9733; &#9733; &#9733; &#9734;";
+                
+                
+
+
+            cardFooter.appendChild(small);
+            cardBody.appendChild(namebox);
+            cardBody.appendChild(pricetag);
+            cardBody.appendChild(descBox);
+            card.appendChild(link);
+            card.appendChild(cardBody);
+            card.appendChild(cardFooter);
+            cardBox.appendChild(card);
+            products.appendChild(cardBox);
+        }
+      };
+
+    let loadBottles = function() {
+        let item = 'bottle';
+        let qty = 5;
+        let names = [];
+        let desc = [];
+    
+        names[0] = 'Glass Hydro Tracker';
+        names[1] = 'Clip on Bottle';
+        names[2] = 'Classic Companion';
+        names[3] = 'Hiking Bottle';
+        names[4] = 'Simple Mason Jar';
+    
+        desc[0] = 'Ensure you get your daily intake with this handy bottle!';
+        desc[1] = '100% recycled bottle. Comes with a clip';
+        desc[2] = 'Sturdy metal bottle. Comes with a handle';
+        desc[3] = 'Studry metal bottle. No handle';
+        desc[4] = 'For all your iced cofee and tea needs';
+    
+        let prices = [];
+        prices[0] = '$29.99';
+        prices[1] = '$9.99';
+        prices[2] = '$19.99';
+        prices[3] = '$14.99';
+        prices[4] = '$7.99';
+    
+        loadItems(item, prices, names, desc, qty);
+        //at the end i need to append card to cardbox and append cardbox to productsContent
+    };
+
+    function loadUtensils() {
+        let item = 'utensil';
+        let qty = 3;
+        let names = [];
+        let desc = [];
+    
+        names[0] = 'Wooden Cutlery';
+        names[1] = 'Metal Straw Set';
+        names[2] = 'Cooking Utensils';
+    
+        desc[0] = 'Safe to use. 100% natural wood for meals';
+        desc[1] = '3 10" metal straws. Comes with a cleaner';
+        desc[2] = 'Safe to use. 100% natural wood for cooking';
+    
+        let prices = [];
+        prices[0] = '$29.99';
+        prices[1] = '$20.99';
+        prices[2] = '$20.99';
+    
+        loadItems(item, prices, names, desc, qty);
     }
-  };
 
-  function loadBaskets() {
+    function loadBaskets() {
     let item = 'basket';
     let qty = 2;
     let names = [];
@@ -66,162 +138,63 @@
     prices[1] = '$4.99';
 
     loadItems(item, prices, names, desc, qty);
-  }
-
-  let loadBottles = function() {
-    let item = 'bottle';
-    let qty = 5;
-    let names = [];
-    let desc = [];
-
-    names[0] = 'Glass Hydro Tracker';
-    names[1] = 'Clip on Bottle';
-    names[2] = 'Classic Companion';
-    names[3] = 'Hiking Bottle';
-    names[4] = 'Simple Mason Jar';
-
-    desc[0] = 'Ensure you get your daily intake with this handy bottle!';
-    desc[1] = '100% recycled bottle. Comes with a clip';
-    desc[2] = 'Sturdy metal bottle. Comes with a handle';
-    desc[3] = 'Studry metal bottle. No handle';
-    desc[4] = 'For all your iced cofee and tea needs';
-
-    let prices = [];
-    prices[0] = '$29.99';
-    prices[1] = '$9.99';
-    prices[2] = '$19.99';
-    prices[3] = '$14.99';
-    prices[4] = '$7.99';
-
-    loadItems(item, prices, names, desc, qty);
-    //at the end i need to append card to cardbox and append cardbox to productsContent
-  };
-
-  function loadUtensils() {
-    let item = 'utensil';
-    let qty = 3;
-    let names = [];
-    let desc = [];
-
-    names[0] = 'Wooden Cutlery';
-    names[1] = 'Metal Straw Set';
-    names[2] = 'Cooking Utensils';
-
-    desc[0] = 'Safe to use. 100% natural wood for meals';
-    desc[1] = '3 10" metal straws. Comes with a cleaner';
-    desc[2] = 'Safe to use. 100% natural wood for cooking';
-
-    let prices = [];
-    prices[0] = '$29.99';
-    prices[1] = '$20.99';
-    prices[2] = '$20.99';
-
-    loadItems(item, prices, names, desc, qty);
-  }
-
-  function setupMenuHandlers() {
-    let all = document.querySelector('#All');
-    all.onclick = function() {
-      clearProducts();
-      loadBottles();
-      loadBaskets();
-      loadUtensils();
-      clearUnderline();
-      let temp = document.querySelector('#uText0');
-      temp.style.textDecoration = 'underline';
-    };
-
-    let bottles = document.querySelector('#Bottles');
-    bottles.onclick = function() {
-      clearProducts();
-      loadBottles();
-      clearUnderline();
-      let temp = document.querySelector('#uText1');
-      temp.style.textDecoration = 'underline';
-    };
-
-    let baskets = document.querySelector('#Baskets');
-    baskets.onclick = function() {
-      clearProducts();
-      loadBaskets();
-      clearUnderline();
-      let temp = document.querySelector('#uText2');
-      temp.style.textDecoration = 'underline';
-    };
-
-    let utensils = document.querySelector('#Utensils');
-    utensils.onclick = function() {
-      clearProducts();
-      loadUtensils();
-      clearUnderline();
-      let temp = document.querySelector('#uText3');
-      temp.style.textDecoration = 'underline';
-    };
-  }
-  function start() {
-    setupMenuHandlers();
-    loadBaskets();
-    loadBottles();
-    loadUtensils();
-    loadLinks();
-    loadbuttons();
-  }
-
-  function clearProducts() {
-    let content = document.querySelector('.productsContent');
-    let flag = true;
-
-    while (flag === true) {
-      let card = document.querySelector('.cardBox');
-      console.log('card');
-      if (card !== null) {
-        content.removeChild(card);
-      } else {
-        flag = false;
-      }
     }
-  }
 
-  function clearUnderline() {
-    let u0 = document.querySelector('#uText0');
-    u0.style.textDecoration = 'none';
+    function clearProducts() {
+        let content = document.querySelector('#products');
+        let flag = true;
+        content.innerHTML="";
 
-    let u1 = document.querySelector('#uText1');
-    u1.style.textDecoration = 'none';
+      }
 
-    let u2 = document.querySelector('#uText2');
-    u2.style.textDecoration = 'none';
+    function setupMenuHandlers() {
+        
+        let all = document.querySelector('#All');
+        all.onclick = function() {
+            clearProducts();
+            loadBaskets();
+            loadBottles();
+            loadUtensils(); 
+            let listings = document.querySelector('.my-4');
+            listings.innerHTML="All Products";
+        };
 
-    let u3 = document.querySelector('#uText3');
-    u3.style.textDecoration = 'none';
-  }
+        let bottles = document.querySelector('#Bottles');
+        bottles.onclick = function() {
+        clearProducts();
+        loadBottles();
+        let listings = document.querySelector('.my-4');
+        listings.innerHTML="Bottles";
 
-  function loadLinks() {
-    let f = document.querySelector('#facebook');
-    f.setAttribute('href', 'references.html');
+        };
 
-    let i = document.querySelector('#instagram');
-    i.setAttribute('href', 'references.html');
-  }
+        let baskets = document.querySelector('#Baskets');
+        baskets.onclick = function() {
+        clearProducts();
+        loadBaskets();
+        let listings = document.querySelector('.my-4');
+        listings.innerHTML="Baskets";
 
-  function loadbuttons() {
-    let temp1 = document.querySelector('#question');
-    temp1.onclick = function() {
-      let field = document.querySelector('#orderNumber');
-      field.setAttribute('type', 'text');
-    };
-  }
+        };
 
-  window.onload = function() {
+        let utensils = document.querySelector('#Utensils');
+        utensils.onclick = function() {
+        clearProducts();
+        loadUtensils();
+        let listings = document.querySelector('.my-4');
+        listings.innerHTML="Utensils";
+        };
+    }
+
+    function start() {
+        setupMenuHandlers();
+        loadBaskets();
+        loadBottles();
+        loadUtensils();  
+    }
+
+    window.onload = function() {
     start();
-  };
+    };
+
 })();
-
-//References
-
-//https://www.123rf.com/photo_106721134_eco-day-use-shopping-bag-with-vegetables-grocery-shopping-in-supermarket.html
-//https://unsplash.com/photos/-lHZUkiWM74
-//https://unsplash.com/photos/acKeoUAeAd8
-//Logo from https://hatchful.shopify.com/
-//Facebook icon https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F342116%2Ffacebook_media_set_shadow_social_icon&psig=AOvVaw1JGfhlH1WpguQiDWKF8LND&ust=1586304913581000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMCd4LqE1egCFQAAAAAdAAAAABAD
-//Instagram icon https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonscout.com%2Ficon%2Finstagram-1865894&psig=AOvVaw3byKgo-NZTutst2mOvKc9R&ust=1586304962102000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMj5s9GE1egCFQAAAAAdAAAAABAD
